81ae8ee645c40d070e9630594f4330defaad158f
Hicham Guessab <hichamgsb.pro@gmail.com>
2025-01-05 23:09:10 +0100

    add skill-section component

---
+++ src/app/components/skills-section/skills-section.component.ts
@@ -0,0 +1,26 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { SkillCategoryComponent } from './skill-category/skill-category.component';
+import { SkillService } from '../../services/skill.service';
+import { Skill } from '../../interfaces';
+
+@Component({
+  selector: 'skills-section',
+  templateUrl: './skills-section.component.html',
+  standalone: true,
+  imports: [SkillCategoryComponent],
+})
+export class SkillsSectionComponent implements OnInit {
+  designSkills: Skill[] = [];
+  frontEndSkills: Skill[] = [];
+  backEndSkills: Skill[] = [];
+  toolsSkills: Skill[] = [];
+
+  skillService: SkillService = inject(SkillService);
+
+  ngOnInit() {
+    this.designSkills = this.skillService.getDesignSkills();
+    this.frontEndSkills = this.skillService.getFrontEndSkills();
+    this.backEndSkills = this.skillService.getBackEndSkills();
+    this.toolsSkills = this.skillService.getToolsSkills();
+  }
+}
