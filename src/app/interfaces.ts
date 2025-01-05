031370c8de584a8eab4820e17221feefc9b11ae3
Hicham Guessab <hichamgsb.pro@gmail.com>
2025-01-05 23:03:12 +0100

    add enum and interfaces for Skills and Images

---
+++ src/app/interfaces.ts
@@ -0,0 +1,55 @@
+export interface Skill {
+  name: string;
+  image: Image;
+  type: SkillType;
+}
+
+export enum SkillType {
+  Default = 'default',
+  Shiny = 'shiny',
+}
+
+export type Image = BlueImage | ShinyImage | WhiteImage;
+
+export enum BlueImage {
+  About = 'assets/images/blue-about.png',
+  At = 'assets/images/blue-at.png',
+  Directory = 'assets/images/blue-directory.png',
+  Github = 'assets/images/blue-github.png',
+  Identity = 'assets/images/blue-identity.png',
+  Linkedin = 'assets/images/blue-linkedin.png',
+  Stack = 'assets/images/blue-stack.png',
+  Study = 'assets/images/blue-study.png',
+  Technology = 'assets/images/blue-technology.png',
+  Work = 'assets/images/blue-work.png',
+}
+
+export enum ShinyImage {
+  Angular = 'assets/images/shiny-angular.png',
+  Figma = 'assets/images/shiny-figma.png',
+  Tailwind = 'assets/images/shiny-tailwind.png',
+  Typescript = 'assets/images/shiny-typescript.png',
+}
+
+export enum WhiteImage {
+  Angular = 'assets/images/white-angular.png',
+  Bash = 'assets/images/white-bash.png',
+  Docker = 'assets/images/white-docker.png',
+  Express = 'assets/images/white-express.png',
+  Figma = 'assets/images/white-figma.png',
+  Flutter = 'assets/images/white-flutter.png',
+  Git = 'assets/images/white-git.png',
+  Gitlab = 'assets/images/white-gitlab.png',
+  Ice = 'assets/images/white-ice.png',
+  Ionic = 'assets/images/white-ionic.png',
+  Java = 'assets/images/white-java.png',
+  Mongodb = 'assets/images/white-mongodb.png',
+  Nodejs = 'assets/images/white-nodejs.png',
+  Npm = 'assets/images/white-npm.png',
+  Pgsql = 'assets/images/white-pgsql.png',
+  Python = 'assets/images/white-python.png',
+  ReactNative = 'assets/images/white-reactNative.png',
+  Sql = 'assets/images/white-sql.png',
+  Tailwind = 'assets/images/white-tailwind.png',
+  Typescript = 'assets/images/white-typescript.png',
+}
